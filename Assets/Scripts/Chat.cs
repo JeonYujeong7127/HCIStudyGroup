e02d6594853c9b693f41b2d7238962d272099dee
lsjsos <lsjsos@konkuk.ac.kr>
2023-05-29 21:40:52 +0900

    pre_completion
    pre

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chat : MonoBehaviour
+{
+    TextMesh chatField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        chatField.text += "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
