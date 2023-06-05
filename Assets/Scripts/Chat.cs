f6c0a90c7929f9b733ae05a561dcc26a02e8a2b1
lsjsos <lsjsos@konkuk.ac.kr>
2023-06-06 00:06:38 +0900

    change UI Colors.
    changed UI colors.

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -21,9 +21,8 @@ public class Chat : MonoBehaviour
 
     public void Chatting()
     {
-        
         TMP_InputField inputText = inputField.GetComponent<TMP_InputField>();
-        chatText.SetText(chatText.GetParsedText().Split("\n")[1]+"\n"+inputText.text);
+        chatText.SetText(chatText.GetParsedText().Split("\n")[1] + "\n" + inputText.text);
         inputText.SetTextWithoutNotify("");
     }
 }
